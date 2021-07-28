927c400358a676b4a2b16c2112c3704e8a5ee752
avpizarro <avpizarro@live.com>
2021-07-28 18:32:23 +1000

    Add upload image route

---
+++ server/utils/params-config.js
@@ -0,0 +1,18 @@
+// Require the unique identifier package to be used to create an unique name for
+// the uploaded file
+const { v4: uuidv4 } = require('uuid');
+
+const params = fileName => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+  
+    const imageParams = {
+      Bucket: 'user-images-768480db-abc5-4aef-80cd-08e44b914c45',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer
+    };
+  
+    return imageParams;
+  };
+
+  module.exports = params;
